d7db42eb9290ac341f20983c721eb11060d61553
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-03-01 15:49:40 -0600

    Updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import resume from '../images/resume.png';
+import resume1 from '../images/resume-1.png';
+import resume2 from '../images/resume-2.png';
 import resumePDF from '../resume.pdf';
 
 function Resume() {
   return (
     <div id="resume">
       <a href={resumePDF} download="resume">Download PDF</a>
-      <img src={resume} alt="Resume"></img>
+      <img src={resume1} alt="Resume"></img>
+      <img src={resume2} alt="Resume"></img>
     </div>
   );
 }
